9efb1d043280e0ca65c273ecfc4c03e382bd8e25
ajiteshkumar9315 <ajiteshkumar9315888@gmail.com>
2023-07-22 21:08:21 +0530

    adding firebase, changing background color, adding alert function

--- src/Component/RegisterForm.js
+++ src/Component/RegisterForm.js
@@ -7,7 +7,7 @@ const RegisterForm = () => {
     lastName:"",
     email:"",
     city:"",
-    zip:"",
+    Phone:"",
   });
   
   let name, value;
@@ -18,26 +18,72 @@ const RegisterForm = () => {
     setUser({ ...user, [name]:value});
   };
 
+  const postData = async(e) => {
+    e.preventDefault();
+    
+    //object destructuring
+    const {firstName,lastName,email,city,Phone} = user;
+
+    if (firstName && lastName && email && city && Phone){
+
+   
+    
+    //connection,define content-type,change json to string for body
+    // while adding the url you have to define the name of database with extension of .json.
+    // define method,header.
+    const res = await fetch(
+      "https://fir-tut1-e5ca0-default-rtdb.firebaseio.com/tutDatabase.json",{
+        method: "POST",
+        headers:{
+          "Content-type":"application/json",
+        },
+        body:JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          city,
+          Phone,
+        }),
+      }
+    );
+
+    if(res){
+      setUser({
+        firstName:"",
+        lastName:"",
+        email:"",
+        city:"",
+        Phone:"",
+      });
+
+      alert("Data Store Sucessfully");
+    }
+  }
+  else{
+    alert("Please Enter The Data");
+  }
+  };
+
   return (
     <div className='contan '>
-      <form className="row ">
+      <form className="row " method='POST'>
   <div className="col-md-5 m-3 mx-4">
     <label htmlFor="validationDefault01" className="form-label p-3">First name</label>
-    <input type="text" className="form-control" id="validationDefault01" name="firstName" value={user.firstName} onChange={getUserData} autoComplete='off' required/>
+    <input type="text" className="form-control" id="validationDefault01" name="firstName" value={user.firstName} onChange={getUserData} autoComplete='off' placeholder='Enter The FirstName' required/>
   </div>
   <div className="col-md-5 m-3">
     <label htmlFor="validationDefault02" className="form-label p-3">Last name</label>
-    <input type="text" className="form-control" id="validationDefault02" name="lastName" value={user.lastName} onChange={getUserData} autoComplete='off'  required/>
+    <input type="text" className="form-control" id="validationDefault02" name="lastName" value={user.lastName} onChange={getUserData} autoComplete='off' placeholder='Enter The LastName' required/>
   </div>
   <div className="col-md-5 m-3 mx-4">
     <label htmlFor="validationDefaultUsername" className="form-label p-3">Email</label>
     <div className="input-group">
-      <input type="email" className="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" name='email' value={user.email} onChange={getUserData} autoComplete='off'required/>
+      <input type="email" className="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" name='email' value={user.email} onChange={getUserData} autoComplete='off' placeholder='Enter The Email' required/>
     </div>
   </div>
   <div className="col-md-5 m-3 mx-4">
     <label htmlFor="validationDefault03" className="form-label p-3">City</label>
-    <input type="text" className="form-control" id="validationDefault03" name='city' value={user.city} onChange={getUserData} autoComplete='off'  required/>
+    <input type="text" className="form-control" id="validationDefault03" name='city' value={user.city} onChange={getUserData} autoComplete='off' placeholder='Enter The city' required/>
   </div>
   {/* <div className="col-md-3">
     <label htmlFor="validationDefault04" className="form-label">State</label>
@@ -47,12 +93,12 @@ const RegisterForm = () => {
     </select>
   </div> */}
   <div className="col-md-5 m-3 mx-4">
-    <label htmlFor="validationDefault05" className="form-label p-3">Zip</label>
-    <input type="text" className="form-control" id="validationDefault05" name='zip' value={user.zip} onChange={getUserData} autoComplete='off'  required/>
+    <label htmlFor="validationDefault05" className="form-label p-3">Mobile Number</label>
+    <input type="text" className="form-control" id="validationDefault05" name='Phone' value={user.Phone} onChange={getUserData} autoComplete='off' placeholder='Enter The Mobile Number' required/>
   </div>
  
   <div className="col-12 m-5">
-    <button className="btn btn-dark w-25 h-100 rounded-pill" type="submit">Submit </button>
+    <button className="btn btn-dark w-25 h-100 rounded-pill" type="submit" onClick={postData}>Submit </button>
   </div>
 </form>
     </div>
